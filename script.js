f3a711823453d6d7581c72263d0c1482da680f4e
Brucaraujo777 <pyuabrasil@gmail.com>
2023-06-15 17:50:11 -0300

    Projeto6-Dev_Sorteio

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function generateNumber() {
+
+    const min = Math.ceil(document.querySelector(".min").value)
+    const max = Math.floor(document.querySelector(".max").value)
+
+    const result = Math.floor(Math.random() * (max - min + 1) + min)
+
+    console.log(result) 
+
+    alert(result)
+}
